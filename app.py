7d8926362796516f1c2ec2f469cbb14178e4b274
CODESUIT05 <rolfin79@gmail.com>
2025-05-14 00:20:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from PIL import Image
 import hashlib
 import exifread # type: ignore
 import io
+import os
 
 app = Flask(__name__)
 
@@ -57,4 +58,6 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
